0571ceb3b44912bc27d767fa60697f20bd2dc54b
Nabil <20hussan@elmbrookstudents.org>
2018-02-16 14:37:15 -0600

    Modified TestClass

--- src/iOStuff/TestIO.java
+++ src/iOStuff/TestIO.java
@@ -1,5 +1,11 @@
 package iOStuff;
-
+import java.util.Scanner;
+import java.io.*;
 public class TestIO {
-
+	public static void main(String[] args){
+		System.out.println("Please input datafile name here.");
+		Scanner input = new Scanner(System.in);
+		String fileName = input.nextLine();
+		File file = new File(fileName);
+	}
 }
